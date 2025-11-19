4a31659df28a715e4bbe4e211ba7c209b144bc51
bs-comscience <comscience@bsnamil.hs.kr>
2025-11-19 21:04:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ level = st.selectbox("대상 학교급/학년", school_levels)
 grammer_list = [ "to 부정사", "관계대명사"]
 grammer = st.multiselect("핵심 문법(중복 선택 가능)", grammer_list)
 
-
-
 count = st.text_input("문장 생성 개수")
 
 # --- 실행 버튼 ---
@@ -33,7 +31,7 @@ if st.button("문장 만들기"):
     # 원본 코드의 규칙 그대로 유지
     lexile_level = level_to_lexile_map[level]
 
-    prompt = f"영단어 {word}를 사용, 어휘수준 {level}에 맞는, {grammer}를 포함한, 문장 {count}개 생성"
+    prompt = f"영단어 {word}를 사용, 어휘수준 {level}에 맞는, {grammer}를 포함한, 문장 rmsi{count}개 생성"
 
     response = client.chat.completions.create(
         model="gpt-4.1",
