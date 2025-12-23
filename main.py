424e8e5fdfe34a8b362ac31ba03e9ec6a04c340c
bs-comscience <comscience@bsnamil.hs.kr>
2025-12-23 09:06:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image
 # 브라우저 제목 변경
 st.set_page_config(
     page_title="융합탐구반 온라인 전시 사이트",
-    page_icon="assets/mark.png"
+    page_icon="mark.png"
 )
 st.link_button("네이버 가기", "https://www.naver.com")
 
