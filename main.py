c261f02961cfdb8fdc126988e2acc9b14424381c
bs-comscience <comscience@bsnamil.hs.kr>
2025-12-24 08:38:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from PIL import Image
 
-
+'''
 <a href="https://ai-service-exam-avcnjfj9hoj79ldi7x96ba.streamlit.app" 
    oncopy="event.clipboardData.setData('text/plain', '융합탐구반 온라인 전시'); event.preventDefault();">
     융합탐구반 온라인 전시
 </a>
-
+'''
         
 # 브라우저 제목 변경
 st.set_page_config(
