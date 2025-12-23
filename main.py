a6a74ab4164e993d42316a1f5bc83d39b9fc0f3a
bs-comscience <comscience@bsnamil.hs.kr>
2025-12-23 09:21:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ st.set_page_config(
 
 st.write("융합탐구반 온라인 전시 사이트입니다.")
 
+st.markdown("""
+<style>
+/* link_button 스타일 고정 */
+a[data-testid="stLinkButton"] {
+    width: 220px !important;
+    height: 50px !important;
+    display: inline-flex !important;
+    justify-content: center !important;
+    align-items: center !important;
+    font-size: 16px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
 st.link_button("인터넷 작명소", "https://www.naver.com")
 st.link_button("영어 문장 자동 생성", "https://www.naver.com")
 st.link_button("화학....", "https://www.naver.com")
